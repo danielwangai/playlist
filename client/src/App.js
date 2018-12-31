ebe8a76fa4569d13e185abbf2a5d0655c7af487b
Daniel Wangai <mainad57@gmail.com>
2018-12-31 12:55:30 +0300

    add react client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Readers list</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
