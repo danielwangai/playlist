2faf4dda056deb6619a9c3d737520a2489dc46ea
Daniel Wangai <mainad57@gmail.com>
2018-12-31 11:02:29 +0300

    add mutations to save author and book data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,8 @@
 const graphql = require("graphql")
 
+const Book = require("../models/book")
+const Author = require("../models/author")
+
 // destructuring
 const {
     GraphQLObjectType,
@@ -91,6 +94,45 @@ const RootQuery = new GraphQLObjectType({
     }
 })
 
+// mutations
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: {type: GraphQLString},
+                age: { type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                // implememnt validations here
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                })
+                return author.save()
+            }
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: { type: GraphQLString },
+                genre: { type: GraphQLString },
+                authorId: { type: GraphQLString }
+            },
+            resolve(parent, args) {
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId
+                })
+                return book.save()
+            }
+        }
+    }
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 })
