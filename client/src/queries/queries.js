57e0e13c0317fdb58985ba7a5e6c334085dce390
Daniel Wangai <mainad57@gmail.com>
2019-01-02 14:44:51 +0300

    - create state variable - create event listeners to update state - add compose function to group graphql queries - add mutation to create new books - define mutation variables to dynamically create books

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -23,3 +23,18 @@ export const getBooksQuery = gql`
     }
 }
 `
+
+/**
+ * get variables and pass them into mutations
+ * mutation variables - define data types e.g. string e.t.c
+ * ! sign defines that variable cannot be null
+ */
+export const addBookMutation = gql`
+    mutation($name: String!, $genre: String!, $authorId: String!) {
+        addBook (name: $name, genre: $genre, authorId: $authorId) {
+            id
+            name
+            genre
+        }
+    }
+`
