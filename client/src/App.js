d9bdf9f5dc101bee46409ad3162af5abab6c26b5
Daniel Wangai <mainad57@gmail.com>
2019-01-02 12:24:24 +0300

    - setup react apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import ApolloClient from "apollo-boost";
+import {ApolloProvider } from "react-apollo";
+
+import Booklist from "./components/Booklist";
+
+const client = new ApolloClient({
+  uri: process.env.APOLLO_CLIENT_URI
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Readers list</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="main">
+          <h1>Readers list</h1>
+          <Booklist />
+        </div>
+      </ApolloProvider>
     );
   }
 }
